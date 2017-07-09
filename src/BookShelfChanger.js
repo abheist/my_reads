e1956b7f67a1c9f2cbdd795409f9809b9c4e30fc
Abhishek Kumar Singh <abhishek@kisanhub.com>
2017-07-10 00:07:36 +0530

    refactor: book list

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -6,9 +6,7 @@ class BookShelfChanger extends React.Component {
             <div className="book-shelf-changer">
                 <select>
                     <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently
-                        Reading
-                    </option>
+                    <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
                     <option value="read">Read</option>
                     <option value="none">None</option>
