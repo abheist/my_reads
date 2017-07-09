58bbfff91f8e87500ed7e0fa2a37b554f495ffc4
Abhishek Kumar Singh <abhishek@kisanhub.com>
2017-07-09 23:16:21 +0530

    refactor: book with props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Link, Route} from "react-router-dom";
 import BookShelfChanger from "./BookShelfChanger";
 import SearchBook from "./SearchBook";
+import Book from "./Book";
 import "./App.css";
 
 class App extends React.Component {
@@ -11,9 +12,7 @@ class App extends React.Component {
             <div className="app">
                 <Route
                     path="/search"
-                    render={() => (
-                        <SearchBook />
-                    )}
+                    render={() => (<SearchBook />)}
                 />
 
                 <Route path="/"
@@ -30,18 +29,11 @@ class App extends React.Component {
                                            <div className="bookshelf-books">
                                                <ol className="books-grid">
                                                    <li>
-                                                       <div className="book">
-                                                           <div className="book-top">
-                                                               <div className="book-cover" style={{
-                                                                   width: 128,
-                                                                   height: 193,
-                                                                   backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
-                                                               }}></div>
-                                                               <BookShelfChanger/>
-                                                           </div>
-                                                           <div className="book-title">To Kill a Mockingbird</div>
-                                                           <div className="book-authors">Harper Lee</div>
-                                                       </div>
+                                                       <Book
+                                                           bookImage="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+                                                           bookName="To Kill a Mockingbird"
+                                                           bookAuthor="Harper Lee"
+                                                       />
                                                    </li>
                                                    <li>
                                                        <div className="book">
---
+++ src/Book.js
@@ -0,0 +1,23 @@
+import React from "react";
+import BookShelfChanger from "./BookShelfChanger";
+
+class Book extends React.Component {
+    render() {
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${this.props.bookImage})`
+                    }}></div>
+                    <BookShelfChanger/>
+                </div>
+                <div className="book-title">{this.props.bookName}</div>
+                <div className="book-authors">{this.props.bookAuthor}</div>
+            </div>
+        )
+    }
+}
+
+export default Book
