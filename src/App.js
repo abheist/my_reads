37084a383c2d5b299373b2a05b3574a54c4a1b79
Abhishek Kumar Singh <abhishek@kisanhub.com>
2017-07-09 18:39:34 +0530

    feat: remove url manager from state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,6 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-    state = {
-        /**
-         * TODO: Instead of using this state variable to keep track of which page
-         * we're on, use the URL in the browser's address bar. This will ensure that
-         * users can use the browser's back and forward buttons to navigate between
-         * pages, as well as provide a good URL they can bookmark and share.
-         */
-        showSearchPage: true
-    };
-
     render() {
         return (
             <div className="app">
