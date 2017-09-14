885b59e027d3982ef2ca49d2b1c7ed5f4fe3ab83
Abhishek Kumar Singh <abhishek@kisanhub.com>
2017-09-14 23:05:00 +0530

    AKS: use of stateless functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,26 +1,25 @@
 import React from "react";
 import Book from "./Book";
 
-class BookShelf extends React.Component {
-    render() {
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.shelfName}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.props.books.map((book) => {
-                            return <li key={book.title}>
-                                <Book book={book}
-                                    changeBookShelf={this.props.changeBookShelf}
-                                    booksInShelf={this.props.booksInShelf}
-                                />
-                            </li>
-                        })}
-                    </ol>
-                </div>
+
+const BookShelf = (props) => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{props.shelfName}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {props.books.map((book) => {
+                        return <li key={book.title}>
+                            <Book book={book}
+                                changeBookShelf={props.changeBookShelf}
+                                booksInShelf={props.booksInShelf}
+                            />
+                        </li>
+                    })}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BookShelf
