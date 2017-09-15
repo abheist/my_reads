5a95b755f1db128f12bd760624fdba43afb746e4
Abhishek Kumar Singh <abhishek@kisanhub.com>
2017-09-16 01:39:10 +0530

    AKS: change bookShelfChanger to stateless component

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,22 +1,20 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-class BookShelfChanger extends React.Component {
-    render() {
-        return (
-            <div className="book-shelf-changer">
-                <select
-                    onChange={e => this.props.changeBookShelf(this.props.book, e)}
-                    value={this.props.bookShelf || 'none'}>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
-            </div>
-        )
-    }
+const BookShelfChanger = (props) => {
+    return (
+        <div className="book-shelf-changer">
+            <select
+                onChange={e => props.changeBookShelf(props.book, e)}
+                value={props.bookShelf || 'none'}>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+            </select>
+        </div>
+    )
 }
 
 BookShelfChanger.propTypes = {
